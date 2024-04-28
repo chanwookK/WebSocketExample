fb7a56427bfc205b8609a1eee9268e67902addd1
kangchanwook <kan0202@naver.com>
2024-04-28 23:50:59 +0900

    WebSocket example

---
+++ src/main/java/websocket/demo/domain/Greeting.java
@@ -0,0 +1,13 @@
+package websocket.demo.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/websocket/demo/domain/HelloMessage.java
@@ -0,0 +1,15 @@
+package websocket.demo.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+    private String name;
+}
